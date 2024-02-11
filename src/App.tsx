f4c067c1352f0d16997cd314d9906b1740179663
Mohammed Ahsan <earth.ahsan@gmail.com>
2024-02-12 00:51:29 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+import { CustomKanban } from './CustomKanban';
+
+
+
+const App = () => {
+  
+
+  return (
+   <CustomKanban/>
+  );
+};
+
+export default App;
+
